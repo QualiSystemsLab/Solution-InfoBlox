552f71907fbe599098db3ba7eed1be23be340343
Omri Avraham <omritk@gmail.com>
2021-01-06 14:14:04 -0700

    create fixed ip record, moved view

--- infobloxadmin/src/driver.py
+++ infobloxadmin/src/driver.py
@@ -29,6 +29,50 @@ class InfobloxadminDriver (ResourceDriverInterface):
         """
         pass
 
+    def create_fixed_ip_host_record(self, context, dns_name, ip_address, mac_address):
+        """
+        :param ResourceCommandContext context:
+        :param str ip_address:
+        :param str dns_name:
+        :param str mac_address:
+        :return:
+        """
+        headers = {"ContentType": "application/json", "Accept": "*/*"}
+        api_version = "v2.5"
+        cs_api = CloudShellAPISession(host=context.connectivity.server_address,
+                                      token_id=context.connectivity.admin_auth_token, domain="Global")
+        infoblox_address = context.resource.address
+        infoblox_url = f"https://{infoblox_address}/wapi/{api_version}/record:host"
+        infoblox_username = context.resource.attributes.get(f"{context.resource.model}.User")
+        infoblox_password = cs_api.DecryptPassword(context.resource.attributes.get(
+            f"{context.resource.model}.Password")).Value
+        infoblox_view = context.resource.attributes.get(f"{context.resource.model}.View")
+        infoblox_domain_suffix = context.resource.attributes.get(f"{context.resource.model}.DomainSuffix")
+        if not infoblox_domain_suffix.startswith("."):
+            infoblox_domain_suffix = "." + infoblox_domain_suffix
+        request_body = {}
+        if not dns_name.endswith(infoblox_domain_suffix):
+            dns_name = dns_name + infoblox_domain_suffix
+        request_body["name"] = dns_name
+        request_body["view"] = infoblox_view
+        request_body["ipv4addrs"] = [{"ipv4addr": ip_address}]
+
+        if mac_address:
+            request_body["ipv4addrs"][0]["mac"] = mac_address
+            request_body["ipv4addrs"][0]["configure_for_dhcp"] = True
+        try:
+            cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id, f"Request:\n{request_body}")
+            result = requests.post(infoblox_url, headers=headers, json=request_body,
+                                   auth=HTTPBasicAuth(infoblox_username, infoblox_password), verify=False)
+            content = result.content.decode()
+            if result.status_code > 299:
+                raise Exception(f"InfoBlox returned error code: '{result.status_code}', content: '{content}'")
+            else:
+                cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id,
+                                                       f"InfoBlox configured.\nResult:\n{content}")
+        except Exception as e:
+            raise Exception(f"Error sending request to InfoBlox. Error: {e}")
+
     def create_host_record(self, context, dns_name, ip_address, network_address, mac_address, exclude_range):
         """
         :param ResourceCommandContext context:
@@ -56,6 +100,7 @@ class InfobloxadminDriver (ResourceDriverInterface):
         if not dns_name.endswith(infoblox_domain_suffix):
             dns_name = dns_name + infoblox_domain_suffix
         request_body["name"] = dns_name
+        request_body["view"] = infoblox_view
         if ip_address:
             request_body["ipv4addrs"] = [{"ipv4addr": ip_address}]
         elif network_address:
@@ -69,7 +114,6 @@ class InfobloxadminDriver (ResourceDriverInterface):
                 request_body["ipv4addrs"][0]["ipv4addr"]["_parameters"]["exclude"] = exclude_range.split(",")
         else:
             raise Exception("'IP Address' or 'Network Address' must be supplied")
-        request_body["ipv4addrs"][0]["view"] = infoblox_view
 
         if mac_address:
             request_body["ipv4addrs"][0]["mac"] = mac_address
