f110bd3f172047421e3f3bec94b8a6a252356b8e
Omri Avraham <omritk@gmail.com>
2021-01-06 18:51:11 -0700

    change to infoblox python lib

--- infobloxadmin/src/driver.py
+++ infobloxadmin/src/driver.py
@@ -2,8 +2,8 @@ from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterf
 from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
     AutoLoadAttribute, AutoLoadDetails, CancellationContext
 from cloudshell.api.cloudshell_api import CloudShellAPISession
-import requests
-from requests.auth import HTTPBasicAuth
+from infoblox_client import objects
+from infoblox_client import connector
 
 
 class InfobloxadminDriver (ResourceDriverInterface):
@@ -29,45 +29,24 @@ class InfobloxadminDriver (ResourceDriverInterface):
         """
         pass
 
-    def _infoblox_request(self, context, method, url, body=None):
+    def _infoblox_connector(self, context):
         cs_api = CloudShellAPISession(host=context.connectivity.server_address,
                                       token_id=context.connectivity.admin_auth_token, domain="Global")
 
-        headers = {"ContentType": "application/json", "Accept": "*/*"}
-        api_version = "v2.5"
         infoblox_address = context.resource.address
         infoblox_username = context.resource.attributes.get(f"{context.resource.model}.User")
         infoblox_password = cs_api.DecryptPassword(context.resource.attributes.get(
             f"{context.resource.model}.Password")).Value
-        infoblox_url = f"https://{infoblox_address}/wapi/{api_version}/{url}"
-        method = method.lower()
-        try:
-            cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id, f"URL: '{infoblox_url}'")
-            cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id, f"Method: '{method}'")
-            cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id, f"Body: '{body}'")
-            if method == "get":
-                result = requests.get(infoblox_url, headers=headers, auth=HTTPBasicAuth(infoblox_username,
-                                                                                        infoblox_password),
-                                      verify=False)
-            elif method == "post":
-                result = requests.post(infoblox_url, headers=headers, json=body,
-                                       auth=HTTPBasicAuth(infoblox_username, infoblox_password), verify=False)
-            elif method == "put":
-                raise NotImplementedError
-            elif method == "delete":
-                raise NotImplementedError
-            else:
-                raise Exception(f"Unsupported request function '{method}'")
 
-            content = result.content.decode()
-            if result.status_code > 299:
-                raise Exception(f"InfoBlox returned error code: '{result.status_code}', content: '{content}'")
-            else:
-                cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id,
-                                                       f"InfoBlox request completed successfully.\nResult:\n{content}")
-                return content
+        infoblox_config = {"host": infoblox_address, "username": infoblox_username, "password": infoblox_password,
+                           "ssl_verify": False, "wapi_version": "2.5"}
+        try:
+            cs_api.WriteMessageToReservationOutput(context.reservation.reservation_id,
+                                                   f"Connecting to InfoBlox: '{infoblox_address}'")
+            infoblox_connector = connector.Connector(infoblox_config)
+            return infoblox_connector
         except Exception as e:
-            raise Exception(f"Error sending request to InfoBlox. Error: {e}")
+            raise Exception(f"Error connecting to InfoBlox. Error: {e}")
 
     def create_fixed_ip_host_record(self, context, dns_name, ip_address, mac_address):
         """
@@ -79,62 +58,51 @@ class InfobloxadminDriver (ResourceDriverInterface):
         """
         cs_api = CloudShellAPISession(host=context.connectivity.server_address,
                                       token_id=context.connectivity.admin_auth_token, domain="Global")
-        url = "record:host"
         infoblox_view = context.resource.attributes.get(f"{context.resource.model}.View")
         infoblox_domain_suffix = context.resource.attributes.get(f"{context.resource.model}.DomainSuffix")
         if not infoblox_domain_suffix.startswith("."):
             infoblox_domain_suffix = "." + infoblox_domain_suffix
-        request_body = {}
+
         if not dns_name.endswith(infoblox_domain_suffix):
             dns_name = dns_name + infoblox_domain_suffix
-        request_body["name"] = dns_name
-        request_body["view"] = infoblox_view
-        request_body["ipv4addrs"] = [{"ipv4addr": ip_address}]
 
+        infoblox_conn = self._infoblox_connector(context)
         if mac_address:
-            request_body["ipv4addrs"][0]["mac"] = mac_address
-            request_body["ipv4addrs"][0]["configure_for_dhcp"] = True
-
-        data = self._infoblox_request(context, "post", url, request_body)
+            data = objects.HostRecord.create(infoblox_conn, name=dns_name, view=infoblox_view, ip=ip_address,
+                                             mac=mac_address, configure_for_dhcp=True)
+        else:
+            data = objects.HostRecord.create(infoblox_conn, name=dns_name, view=infoblox_view, ip=ip_address)
         return data
 
-    def create_network_ip_host_record(self, context, dns_name, network_address, mac_address, exclude_range):
+    def create_network_ip_host_record(self, context, dns_name, network_address, mac_address):
         """
         :param ResourceCommandContext context:
-        :param str ip_address:
         :param str network_address:
         :param str dns_name:
         :param str mac_address:
-        :param str exclude_range:
         :return:
         """
         cs_api = CloudShellAPISession(host=context.connectivity.server_address,
                                       token_id=context.connectivity.admin_auth_token, domain="Global")
-        url = "record:host"
         infoblox_view = context.resource.attributes.get(f"{context.resource.model}.View")
         infoblox_domain_suffix = context.resource.attributes.get(f"{context.resource.model}.DomainSuffix")
         if not infoblox_domain_suffix.startswith("."):
             infoblox_domain_suffix = "." + infoblox_domain_suffix
-        request_body = {}
+
         if not dns_name.endswith(infoblox_domain_suffix):
             dns_name = dns_name + infoblox_domain_suffix
-        request_body["name"] = dns_name
-        request_body["view"] = infoblox_view
 
-        # request_body["ipv4addrs"] = [{"ipv4addr": f"func:nextavailableip:{network_address}"}]
-        request_body["ipv4addrs"] = [{"ipv4addr": {"_object_function": "next_available_ip",
-                                                   "_object": "network",
-                                                   "_object_parameters": {"network": network_address},
-                                                   "_parameters": {"num": 1},
-                                                   "_result_field": "ips"}}]
-        if exclude_range:
-            request_body["ipv4addrs"][0]["ipv4addr"]["_parameters"]["exclude"] = exclude_range.split(",")
+        infoblox_conn = self._infoblox_connector(context)
 
+        ava_ip = objects.IPAllocation.next_available_ip_from_cidr(infoblox_view, network_address)
         if mac_address:
-            request_body["ipv4addrs"][0]["mac"] = mac_address
-            request_body["ipv4addrs"][0]["configure_for_dhcp"] = True
+            ip = objects.IP.create(ip=ava_ip, mac=mac_address, configure_for_dhcp=True)
+        else:
+            ip = objects.IP.create(ip=ava_ip)
 
-        data = self._infoblox_request(context, "post", url, request_body)
+        data = objects.HostRecord.create(infoblox_conn, name=dns_name, view=infoblox_view, ip=ip, mac=mac_address,
+                                         configure_for_dhcp=True)
+
         return data
 
     def get_host_record_by_name(self, context, dns_name):
@@ -145,9 +113,9 @@ class InfobloxadminDriver (ResourceDriverInterface):
         """
         cs_api = CloudShellAPISession(host=context.connectivity.server_address,
                                       token_id=context.connectivity.admin_auth_token, domain="Global")
-        url = f"record:host?name~={dns_name}"
 
-        data = self._infoblox_request(context, "get", url)
+        infoblox_conn = self._infoblox_connector(context)
+        data = infoblox_conn.get_object("host:record", {"name": dns_name})
         return data
 
     def get_host_record_by_ip(self, context, ip_address):
@@ -158,22 +126,9 @@ class InfobloxadminDriver (ResourceDriverInterface):
         """
         cs_api = CloudShellAPISession(host=context.connectivity.server_address,
                                       token_id=context.connectivity.admin_auth_token, domain="Global")
-        url = f"ipv4address?ip_address={ip_address}"
 
-        data = self._infoblox_request(context, "get", url)
-        return data
-
-    def get_host_record_by_mac(self, context, mac):
-        """
-        :param ResourceCommandContext context:
-        :param str mac:
-        :return:
-        """
-        cs_api = CloudShellAPISession(host=context.connectivity.server_address,
-                                      token_id=context.connectivity.admin_auth_token, domain="Global")
-        url = f"fixedaddress?_return_fields%2B=mac&mac={mac}"
-
-        data = self._infoblox_request(context, "get", url)
+        infoblox_conn = self._infoblox_connector(context)
+        data = infoblox_conn.get_object("host:record", {"ipv4addr": ip_address})
         return data
 
     def delete_host_record(self, context, dns_name):
